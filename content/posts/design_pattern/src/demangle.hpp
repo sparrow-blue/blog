03453fb808cae19b67826f085d33729ed1724285
sparrow-blue <131469533+sparrow-blue@users.noreply.github.com>
2023-06-16 01:33:02 +0900

    関数コメントの追加

--- content/posts/design_pattern/src/demangle.hpp
+++ content/posts/design_pattern/src/demangle.hpp
@@ -5,6 +5,16 @@
 
 #include <string>
 
+/**
+ * @brief 型名をデマングル化する．
+ *
+ * @param id デマングル化する型のtype_infoオブジェクト
+ * @return std::string デマングル化された型名を表す文字列
+ * @throws std::exception デマングル化が失敗した場合に投げられます
+ *
+ * @details
+ * gcc および clang コンパイラでマングル化された型名をデマングル化します．
+ */
 std::string demangle(const std::type_info& id) {
   int status;
   char* demangled_name = abi::__cxa_demangle(id.name(), 0, 0, &status);
